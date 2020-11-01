bc70a57b059cfbbd5f1b47a82a91583c27272db0
Neko <60512800+DiveshTheReal@users.noreply.github.com>
2020-11-01 13:22:01 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,33 @@
+document.getElementById(id)
+document.getElementsByTagName(name)
+document.getElementsByClassName(name)
+document.createElement(element)
+document.removeChild(element)
+document.appendChild(element)
+document.replaceChild(new, old)
+document.write(text)
+document.anchors	
+document.applets	
+document.baseURI
+document.body	Returns the <body> element	1
+document.cookie	Returns the document's cookie	1
+document.doctype	Returns the document's doctype	3
+document.documentElement	Returns the <html> element	3
+document.documentMode	Returns the mode used by the browser	3
+document.documentURI	Returns the URI of the document	3
+document.domain	Returns the domain name of the document server	1
+document.domConfig	Obsolete. Returns the DOM configuration	3
+document.embeds	Returns all <embed> elements	3
+document.forms	Returns all <form> elements	1
+document.head	Returns the <head> element	3
+document.images	Returns all <img> elements	1
+document.implementation	Returns the DOM implementation	3
+document.inputEncoding	Returns the document's encoding (character set)	3
+document.lastModified	Returns the date and time the document was updated	3
+document.links	Returns all <area> and <a> elements that have a href attribute	1
+document.readyState	Returns the (loading) status of the document	3
+document.referrer	Returns the URI of the referrer (the linking document)	1
+document.scripts	Returns all <script> elements	3
+document.strictErrorChecking	Returns if error checking is enforced	3
+document.title	Returns the <title> element	1
+document.URL	Returns the complete URL of the document	1
